855c115627b371495959c24f8f27fcaf70215c15
Earl Tacogdoy <earl.tacogdoy@fligno.com>
2022-02-03 17:53:00 +0800

    Initial Commit

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <main className="h-screen w-screen bg-slate-50">
+      <nav>
+        
+      </nav>
+    </main>
+  );
+}
+
+export default App;
